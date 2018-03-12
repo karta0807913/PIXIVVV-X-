34ff925772b2c2ae2e6fc0305961ec330095e9dd
karta0807913 <karta0807913@gmail.com>
2018-03-12 23:21:07 +0800

    update functions

--- pixiv.py
+++ pixiv.py
@@ -1,10 +1,12 @@
 ﻿from PIL import Image, ImageTk, ImageDraw, ImageFont
 from bs4 import BeautifulSoup
 import tkinter as tk
+from tkinter import filedialog
 import io
 import threading
 import time
 import os.path
+from os.path import basename
 import urllib.request
 import http.cookiejar
 import http.cookies
@@ -37,7 +39,7 @@ class Application(tk.Frame):
         self.pack()
         self.login()
         self.createWidgets()
-        self.imagesPerPage = 750
+        self.imagesPerPage = 250
         self.makeViewIndex = 0
         self.resetMakeViewFunction = False
         self.makeViewIndexLock = threading.Lock()
@@ -66,6 +68,11 @@ class Application(tk.Frame):
         self.searchButton.grid(row=0, column=8,)
         self.searchButton['command'] = lambda : self.searchStart()
 
+        self.loadFileButton = tk.Button(self)
+        self.loadFileButton['text'] = 'open from file'
+        self.loadFileButton.grid(row=0, column=9,)
+        self.loadFileButton['command'] = lambda : self.openFile()
+
         self.safeModeCheckButtonVar = tk.IntVar()
         self.safeModeCheckButton = tk.Checkbutton(self, variable=self.safeModeCheckButtonVar)
         self.safeModeCheckButton['text'] = 'safe mode'
@@ -77,7 +84,7 @@ class Application(tk.Frame):
         self.r18ModeCheckButton.grid(row=8, column=8,)
 
         self.saveButton = tk.Button(self)
-        self.saveButton['text'] = 'save'
+        self.saveButton['text'] = 'save images'
         self.saveButton.grid(row=8, column=1,)
         self.saveButton['command'] = lambda : threading._start_new_thread(self.saveAllImages, (frame.minBookNum.get(), ));
         
@@ -125,6 +132,41 @@ class Application(tk.Frame):
                 self.canvas.config(width=interior.winfo_reqwidth())
         interior.bind('<Configure>', _configure_interior) #如果視窗大小變化
 
+    def openFile(self) :
+        if self.haveNaxt[1] == True :
+            Mbox("Info", "Operation is not ended")
+            return
+        file = filedialog.askopenfilename(filetypes=(("Json files", "*.json"), ))
+        if file == "":
+            return
+
+        self.makeViewIndexLock.acquire()
+        try :
+            self.illustList = json.load(open(file, 'rt', encoding='utf-8'))
+        except :
+            Mbox("Error", "this is not a json file", 0)
+            self.makeViewIndexLock.release()
+            return
+
+        try:
+            self.illustList[0]['artistName']
+            self.illustList[0]['illustName']
+            self.illustList[0]['illustNum']
+            self.illustList[0]['smallImageFileName']
+            self.illustList[0]['hugeImageFileName']
+            self.illustList[0]['book_num']
+            self.illustList[0]['illustUrl']
+            self.illustList[0]['smallImgUrl']
+        except NameError:
+            Mbox("Error", "file error", 0)
+            self.illustList=[]
+            self.makeViewIndexLock.release()
+            return
+        self.makeViewIndexLock.release()
+        self.input.delete(0, len(self.input.get()))
+        self.input.insert(0, os.path.splitext(basename(file))[0])
+        self._flushPage(1)
+
     def login(self):
 
         def makeOpener(head = {
@@ -144,6 +186,7 @@ class Application(tk.Frame):
             return opener
 
         #login to pixiv
+
         id = 'thisismyxxx24@gmail.com'
         passwd = '3d1e2aehky2qwfui20vz'
 
@@ -238,7 +281,7 @@ class Application(tk.Frame):
         while not self.haveNaxt[1]:
             time.sleep(1)
         self.illustList = sorted(self.illustList, key=getKey, reverse=True)
-        f = open(searchStr + self.fileFloderName + '.txt', 'wt', encoding='utf-8')
+        f = open(searchStr + self.fileFloderName + '.json', 'wt', encoding='utf-8')
         s = json.dumps(self.illustList, ensure_ascii=False)
         f.write(s)
         f.close()
@@ -256,7 +299,9 @@ class Application(tk.Frame):
         if self.imagesPerPage + self.makeViewIndex > len(self.illustList):
             Mbox("Info", "this is the last page", 0)
             return 
+
         self.makeViewIndexLock.acquire()
+        self.canvas.yview_moveto(0)
         self.resetMakeViewFunction = True
         self.makeViewIndex += self.imagesPerPage
         self.makeViewIndexLock.release()
@@ -265,38 +310,44 @@ class Application(tk.Frame):
         if self.makeViewIndex - self.imagesPerPage < 0:
             Mbox("Info", "this is the home page", 0)
             return 
+
         self.makeViewIndexLock.acquire()
+        self.canvas.yview_moveto(0)
         self.resetMakeViewFunction = True
         self.makeViewIndex -= self.imagesPerPage
         self.makeViewIndexLock.release()
 
     def setPage(self, pageIndex):
         if self.imagesPerPage * (pageIndex - 1) < 0 or self.imagesPerPage * (pageIndex - 1) > len(self.illustList) :
-            Mbox("Info", "Error page count", 0)
+            Mbox("Info", "Error page index", 0)
             return
         if self.imagesPerPage * (pageIndex - 1) == self.makeViewIndex :
             return
+        
         self.makeViewIndexLock.acquire()
+        self.canvas.yview_moveto(0)
         self.resetMakeViewFunction = True
         self.makeViewIndex = self.imagesPerPage * (pageIndex - 1)
         self.makeViewIndexLock.release()
 
     def _flushPage(self, pageIndex) :
         if self.imagesPerPage * (pageIndex - 1) < 0 or self.imagesPerPage * (pageIndex - 1) > len(self.illustList) :
-            Mbox("Info", "Error page count", 0)
+            Mbox("Info", "Error page index", 0)
             return
+        
         self.makeViewIndexLock.acquire()
+        self.canvas.yview_moveto(0)
         self.resetMakeViewFunction = True
         self.makeViewIndex = self.imagesPerPage * (pageIndex - 1)
         self.makeViewIndexLock.release()
 
     def makeView(self):
 
-        def loadImages(i, index):
-            if (not(os.path.isfile(self.illustList[i]['smallImageFileName']))):
-                arr = np.asarray(bytearray(self.opener.open(self.illustList[i]['smallImgUrl']).read()), dtype=np.uint8)
+        def loadImages(illustinfo, index):
+            if (not(os.path.isfile(illustinfo['smallImageFileName']))):
+                arr = np.asarray(bytearray(self.opener.open(illustinfo['smallImgUrl']).read()), dtype=np.uint8)
                 img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
-                arr.tofile(self.illustList[i]['smallImageFileName'])
+                arr.tofile(illustinfo['smallImageFileName'])
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 img = Image.fromarray(img)
                 d = ImageDraw.Draw(img)
@@ -304,18 +355,18 @@ class Application(tk.Frame):
                     color=(255)
                 else :
                     color=(255,0,0,0)
-                d.text((img.width / 2, img.height/ 2 * 1.5), str(self.illustList[i]['book_num']), fill=color, font=ImageFont.truetype("arial.ttf", size=40))
+                d.text((img.width / 2, img.height/ 2 * 1.5), str(illustinfo['book_num']), fill=color, font=ImageFont.truetype("arial.ttf", size=40))
                 del d
                 self.tkImage[index] = ImageTk.PhotoImage(img)
                 self.buttons[index]['image'] = self.tkImage[index]
             else:
-                img = Image.open(self.illustList[i]['smallImageFileName'])
+                img = Image.open(illustinfo['smallImageFileName'])
                 d = ImageDraw.Draw(img)
                 if len(img.mode) < 3:
                     color=(255)
                 else :
                     color=(255,0,0,0)
-                d.text((img.width / 2, img.height/ 2 * 1.5), str(self.illustList[i]['book_num']), fill=color, font=ImageFont.truetype("arial.ttf", size=40))
+                d.text((img.width / 2, img.height/ 2 * 1.5), str(illustinfo['book_num']), fill=color, font=ImageFont.truetype("arial.ttf", size=40))
                 del d
                 self.tkImage[index] = ImageTk.PhotoImage(img)
                 self.buttons[index]['image'] = self.tkImage[index]
@@ -337,29 +388,32 @@ class Application(tk.Frame):
                 time.sleep(0.2)
                 continue
 
-            self.makeViewIndexLock.acquire()
-            if self.resetMakeViewFunction :
-                i = 0
-                self.resetMakeViewFunction = False
+            #self.makeViewIndexLock.acquire()
             if i < len(self.buttons):
-                self.buttons[i]['command'] = lambda k = i + self.makeViewIndex : threading._start_new_thread(self.showImage, (self.illustList[k], ))
+                self.buttons[i]['command'] = lambda illustInfo = self.illustList[i + self.makeViewIndex] : threading._start_new_thread(self.showImage, (illustInfo, ))
                 if self.buttons[i]['image'] == '':
                     self.buttons[i]['image'] = self.tkImage[i]
             else:
                 self.buttons.append(tk.Button(self.interior, height=200,
                                                 width=200,
                                                 image=self.tkImage[i],
-                                                command=lambda k = i + self.makeViewIndex : threading._start_new_thread(self.showImage, (self.illustList[k], ))))
-                self.buttons[i].bind("<MouseWheel>", self.canvasMouseWheelEvent)
+                                                command=lambda illustInfo = self.illustList[i + self.makeViewIndex] : threading._start_new_thread(self.showImage, (illustInfo, ))))
                 self.buttons[i].grid(row=int(i/5), column=i%5)
+                self.buttons[i].bind("<MouseWheel>", self.canvasMouseWheelEvent)
+                self.buttons[i].bind("<Button-3>", lambda event, illlustUrl=self.illustList[i + self.makeViewIndex]['illustUrl'] : self.cloneTextToClipboard(illlustUrl))
 
             self.makeViewThreadNumLock.acquire()
             self.imageThreadNum += 1
-            threading._start_new_thread(loadImages, (i + self.makeViewIndex, i))
+            threading._start_new_thread(loadImages, (self.illustList[i + self.makeViewIndex], i))
             self.makeViewThreadNumLock.release()
 
-            self.makeViewIndexLock.release()
+            #self.makeViewIndexLock.release()
             i = i + 1
+
+    def cloneTextToClipboard(self, text) :
+        self.clipboard_clear()
+        self.clipboard_append(text)
+        Mbox("ok", "cloned url", 0)
 
     def singalImage(self, findres):
         arr = np.asarray(bytearray(self.opener.open(findres['data-src']).read()), dtype=np.uint8)
